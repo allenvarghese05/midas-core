5755abfed590033e8f8946e6dfbcbd0728415843
allen <allenv@kis.in>
2025-10-14 21:43:23 -0400

    Task Two done

---
+++ src/main/java/com/jpmc/midascore/consumer/TransactionConsumer.java
@@ -0,0 +1,23 @@
+package com.jpmc.midascore.consumer;
+
+import com.jpmc.midascore.foundation.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransactionConsumer {
+
+    private static final Logger logger = LoggerFactory.getLogger(TransactionConsumer.class);
+
+    @KafkaListener(topics = "${general.kafka-topic}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(Transaction transaction) {
+        logger.info("========================================");
+        logger.info("Received transaction: {}", transaction);
+        logger.info("  Sender ID: {}", transaction.getSenderId());
+        logger.info("  Recipient ID: {}", transaction.getRecipientId());
+        logger.info("  Amount: {}", transaction.getAmount());
+        logger.info("========================================");
+    }
+}
